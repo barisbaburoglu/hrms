9397e6685dd08efc4f32de7d10518d34b8cfb098
barisbaburoglu <barisbaburoglu@gmail.com>
2024-08-10 03:37:49 +0300

    first release

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "2b7bb3affeecf8a1f813642704d86325
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "53e3daf2611d1609f360dc8657085c60",
+"flutter_bootstrap.js": "ae27d686b473a009e07d3bac501bc1b2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
